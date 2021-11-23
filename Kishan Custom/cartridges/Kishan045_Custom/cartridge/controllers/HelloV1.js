3386803fcbeb072f1ecf7986ebc89e95f66bb76b
devprince045 <kishan@cyntexa.com>
2021-11-23 11:22:53 +0530

    my sandbox changes

--- Kishan Custom/cartridges/Kishan045_Custom/cartridge/controllers/HelloV1.js
+++ Kishan Custom/cartridges/Kishan045_Custom/cartridge/controllers/HelloV1.js
@@ -2,9 +2,9 @@
 
 var server = require('server');
 
-server.get('Start', function (req, res, next) {
+server.get('Start', function(req, res, next) {
 
-	res.print('<html><body><h1>Hello World</h1></body></html>');
-    next();	 
+    res.print('<html><body><h1>Hellhiiiiiiiio World</h1></body></html>');
+    next();
 });
 module.exports = server.exports();
